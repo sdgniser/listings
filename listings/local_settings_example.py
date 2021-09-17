3b5cd90fabf15fe6fe4e7478cd9376e0beb9a012
PeithonKing <amukherjeeniser@gmail.com>
2021-09-17 16:34:29 +0530

    restored local_settings_examples.py

---
+++ listings/local_settings_example.py
@@ -0,0 +1,20 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-d3lu+*f$s-7c$6xdf-dfd(uvxd@!=#u@7f_e7df9a3a*k3#z!8'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
